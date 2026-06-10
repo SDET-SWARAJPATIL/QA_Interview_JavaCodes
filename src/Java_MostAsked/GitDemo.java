262deaf96520b1e4c1231e1c201eb78845d9d8e1
Swaraj <namefirst21910896@gmail.com>
2026-06-10 13:50:48 +0530

    added git demo class

---
+++ src/Java_MostAsked/GitDemo.java
@@ -0,0 +1,11 @@
+package Java_MostAsked;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am uploaded");
+
+	}
+
+}
